7bc193c03311bbe998f35d09ab9dc11e00f1fd15
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-24 08:07:24 -0400

    test: lemonade-stand

---
+++ src/edabit/lemonade-stand/lemonade-stand.test.ts
@@ -0,0 +1,10 @@
+import { lemonade } from "./lemonade-stand";
+
+describe("Lemonade Stand", () => {
+  it("should have enough change for every costumer", () => {
+    expect(lemonade([5, 5, 5, 10, 20])).toBe(true);
+    expect(lemonade([5, 5, 10, 10, 20])).toBe(false);
+    expect(lemonade([10, 10])).toBe(false);
+    expect(lemonade([5, 5, 10])).toBe(true);
+  });
+});
