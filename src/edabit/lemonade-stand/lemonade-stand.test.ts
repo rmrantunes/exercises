b3e09d31eb952d2d213f7782afa88d202f62b2f0
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-25 15:12:24 -0400

    typo: customer in limonade-stand

--- src/edabit/lemonade-stand/lemonade-stand.test.ts
+++ src/edabit/lemonade-stand/lemonade-stand.test.ts
@@ -1,7 +1,7 @@
 import { lemonade } from "./lemonade-stand";
 
 describe("Lemonade Stand", () => {
-  it("should have enough change for every costumer", () => {
+  it("should have enough change for every customer", () => {
     expect(lemonade([5, 5, 5, 10, 20])).toBe(true);
     expect(lemonade([5, 5, 10, 10, 20])).toBe(false);
     expect(lemonade([10, 10])).toBe(false);
--- src/edabit/lemonade-stand/lemonade-stand.ts
+++ src/edabit/lemonade-stand/lemonade-stand.ts
@@ -2,8 +2,8 @@ export function lemonade(queue: (5 | 10 | 20)[]) {
   let cash = 0;
   const cost = 5;
 
-  for (const costumer of queue) {
-    const change = costumer - cost;
+  for (const customer of queue) {
+    const change = customer - cost;
     if (change > cash) return false;
     else cash += cost - change;
   }
